f35f467f1aedb93d7ac8a3768ba2c5b7a72f9e1f
pjh5144 <pjh5144@gmail.com>
2017-11-02 14:39:46 -0400

    Add files via upload
    Updating Rmd file and accompanying refurbished xlsx docs

--- Data_Cleaning.R
+++ Data_Cleaning.R
@@ -8,57 +8,58 @@ library(xlsx)
 library(tidyr)
 library(reshape2)
 
+###Cleaning of Medication for Control Purposes###
 #Medication Tables
-meds<-read.xlsx("Vestib_Export_08102017.xlsx",header=TRUE,sheetName="export_Vestib_SOT")
-save(meds,file="Meds_cleaning.RData")
-
-meds_split<-meds[,c(1,6)]
-meds_split$Medication<-sub("PM "," PM:",meds_split$Medication)
-meds_split$Medication<-sub("PM:"," PM:",meds_split$Medication)
-meds_split$Medication<-sub("This morning:","AM:",meds_split$Medication)
-meds_split$Medication<-sub("Today:","AM:",meds_split$Medication)
-meds_split$Medication<-sub("Yesterday"," PM",meds_split$Medication)
-meds_split$Medication<-sub("AM ","AM:",meds_split$Medication)
-meds_split$Medication<-sub("Last night:"," PM:",meds_split$Medication)
-meds_split$Medication<-sub("Taken this morning:","AM:",meds_split$Medication)
-meds_split$Medication<-sub("This :"," PM:",meds_split$Medication)
-meds_split$Medication<-sub("Medications taken this morning:","AM:",meds_split$Medication)
-meds_split$Medication<-sub("Last night","PM:",meds_split$Medication)
-meds_split$Medication<-sub("taken last night","PM:",meds_split$Medication)
-meds_split$Medication<-sub("Last evening","PM:",meds_split$Medication)
-meds_split$Medication<-sub("This Morning","AM:",meds_split$Medication)
-meds_split$Medication<-sub("Taken this am:","AM:",meds_split$Medication)
-
-meds_split<-meds_split%>%
-  separate(Medication,c("AM","PM"),sep="PM")
-
-meds_split$AM<-sub("AM:","",meds_split$AM)
-meds_split$PM<-sub(":","",meds_split$PM)
-
-AM<-meds_split%>%
-  separate(AM,paste0("AM",1:20),sep=",",remove=FALSE)%>%
-  select(-c(AM,PM))%>%
-  gather(Time,value=Meds,-ID)%>%
-  filter(!Meds=="NA")%>%
-  mutate(Time="AM")
-
-PM<-meds_split%>%
-  separate(PM,paste0("PM",1:20),sep=",",remove=FALSE)%>%
-  select(-c(AM,PM))%>%
-  gather(Time, value=Meds,-ID)%>%
-  filter(!Meds=="NA")%>%
-  mutate(Time="PM")
-
-medications<-rbind(PM,AM)
-medications$Meds<-tolower(medications$Meds)
-
-View(table(medications$Meds))
-
-medications$Meds<-gsub("sm denies","",medications$Meds)
-medications$Meds<-sub("and","",medications$Meds)
-medications$Meds<-gsub("none","",medications$Meds)
-medications$Meds<-gsub("taking ","",medications$Meds)
-medications$Meds<-gsub("no medications","",medications$Meds)
+# meds<-read.xlsx("Vestib_Export_08102017.xlsx",header=TRUE,sheetName="export_Vestib_SOT")
+# save(meds,file="Meds_cleaning.RData")
+# 
+# meds_split<-meds[,c(1,6)]
+# meds_split$Medication<-sub("PM "," PM:",meds_split$Medication)
+# meds_split$Medication<-sub("PM:"," PM:",meds_split$Medication)
+# meds_split$Medication<-sub("This morning:","AM:",meds_split$Medication)
+# meds_split$Medication<-sub("Today:","AM:",meds_split$Medication)
+# meds_split$Medication<-sub("Yesterday"," PM",meds_split$Medication)
+# meds_split$Medication<-sub("AM ","AM:",meds_split$Medication)
+# meds_split$Medication<-sub("Last night:"," PM:",meds_split$Medication)
+# meds_split$Medication<-sub("Taken this morning:","AM:",meds_split$Medication)
+# meds_split$Medication<-sub("This :"," PM:",meds_split$Medication)
+# meds_split$Medication<-sub("Medications taken this morning:","AM:",meds_split$Medication)
+# meds_split$Medication<-sub("Last night","PM:",meds_split$Medication)
+# meds_split$Medication<-sub("taken last night","PM:",meds_split$Medication)
+# meds_split$Medication<-sub("Last evening","PM:",meds_split$Medication)
+# meds_split$Medication<-sub("This Morning","AM:",meds_split$Medication)
+# meds_split$Medication<-sub("Taken this am:","AM:",meds_split$Medication)
+# 
+# meds_split<-meds_split%>%
+#   separate(Medication,c("AM","PM"),sep="PM")
+# 
+# meds_split$AM<-sub("AM:","",meds_split$AM)
+# meds_split$PM<-sub(":","",meds_split$PM)
+# 
+# AM<-meds_split%>%
+#   separate(AM,paste0("AM",1:20),sep=",",remove=FALSE)%>%
+#   select(-c(AM,PM))%>%
+#   gather(Time,value=Meds,-ID)%>%
+#   filter(!Meds=="NA")%>%
+#   mutate(Time="AM")
+# 
+# PM<-meds_split%>%
+#   separate(PM,paste0("PM",1:20),sep=",",remove=FALSE)%>%
+#   select(-c(AM,PM))%>%
+#   gather(Time, value=Meds,-ID)%>%
+#   filter(!Meds=="NA")%>%
+#   mutate(Time="PM")
+# 
+# medications<-rbind(PM,AM)
+# medications$Meds<-tolower(medications$Meds)
+# 
+# View(table(medications$Meds))
+# 
+# medications$Meds<-gsub("sm denies","",medications$Meds)
+# medications$Meds<-sub("and","",medications$Meds)
+# medications$Meds<-gsub("none","",medications$Meds)
+# medications$Meds<-gsub("taking ","",medications$Meds)
+# medications$Meds<-gsub("no medications","",medications$Meds)
 
 #write.xlsx(medications,file="Medications.xlsx")
 
@@ -109,29 +110,34 @@ injury_counts<-injury%>%
 
 ##DoT 
 SOT<-workbook_list$export_Vestib_SOT
-SOT<-SOT[,-c(4:12,15:17)]
+SOT<-SOT[,-c(4:13,15:17)]
 
 #History
 Hx<-workbook_list$export_Vestib_History
 Hx<-Hx[,c(1,3,5,7,9,11)]
-Hx[,c(2:6)]<-ifelse(!is.na(Hx[,c(2:6)]),"NA","X")
+Hx[,c(2:6)]<-ifelse(!is.na(Hx[,c(2:6)]),"X","NA")
 
 #Vestib Testing
 Testing<-workbook_list$export_Vestib_Combined
 
+Testing<-Testing[,grep("\\_Abnormal|\\ID|\\_Normal",names(Testing))]
+
+Testing$na1<-rowSums(is.na(Testing))
+Testing$na2<-rowSums(Testing[]=="NA")
+Testing$na3<-rowSums(is.na(Testing)|Testing[]=="NA")
+
+Testing<-Testing%>%
+  filter(na3<58) #Review NA's before subsetting. 
+
 Testing<-Testing[,grep("\\_Abnormal|\\ID",names(Testing))]
-Testing$Spon_Gaze<-ifelse(Testing$Vestibular_Oculomotor_Spontaneous_Nystagmus_Abnormal=="C"|Testing$Vestibular_Oculomotor_Gaze_Center_Abnormal=="C","C","X")
+Testing$Spon_Gaze<-ifelse(Testing$Vestibular_Oculomotor_Spontaneous_Nystagmus_Abnormal=="C"|Testing$Vestibular_Oculomotor_Gaze_Center_Abnormal=="C","C","NA")
 Testing$GazeRL<-ifelse(Testing$Vestibular_Oculomotor_Gaze_Left_Abnormal=="C"& Testing$Vestibular_Oculomotor_Gaze_Right_Abnormal=="C","C","NA")
 Testing$GazeR<-ifelse(Testing$Vestibular_Oculomotor_Gaze_Right_Abnormal=="C"& !Testing$Vestibular_Oculomotor_Gaze_Left_Abnormal=="C",Testing$Vestibular_Oculomotor_Gaze_Right_Abnormal,"NA")
 Testing$GazeL<-ifelse(Testing$Vestibular_Oculomotor_Gaze_Left_Abnormal=="C"& !Testing$Vestibular_Oculomotor_Gaze_Right_Abnormal=="C",Testing$Vestibular_Oculomotor_Gaze_Left_Abnormal,"NA")
 
-Testing$Total_C<-rowSums(Testing[,c(3:7,11:34)]=="C")
+Testing$Total_C<-rowSums(Testing[,c(3:7,11:34)]=="C",na.rm = TRUE)
 
 #save(Testing,file="Vestib_Testing.RData")
-
-Testing$na<-rowSums(is.na(Testing))
-Testing<-Testing%>%
-  filter(!na==34)
 
 mymerge.x<-function(x,y){
   merge(x,y,all.x=TRUE,by="ID")
@@ -139,10 +145,8 @@ mymerge.x<-function(x,y){
 
 demo<-workbook_list$export_Demographics
 
-
 final_df<-Reduce(mymerge.x,list(Testing,SOT,Hx,injury_counts,Surveys,demo))
 save(final_df,file="final_df.RData")
-
 
 ##UPDATE 10.25.17 MEDICATIONS##
 pt_meds<-read.xlsx("Control_Medications.xlsx",header=TRUE,sheetIndex = 1)
